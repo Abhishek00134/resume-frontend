451cc6943bf3d5e2b5e604b2b58722c1d2c6c5aa
Abhishek <abhishek346a@gmail.com>
2026-04-29 23:44:19 +0530

    aligned icons

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -111,6 +111,8 @@ const Dashboard = () => {
                 <button key={index} onClick={()=> navigate(`/app/builder/${resume._id}`)} className='relative w-full sm:max-w-36 h-48 flex items-center justify-center rounded-lg gap-2 border group hover:shadow-lg transition-all duration-300 cursor-pointer' style={{background:`linear-gradient(135deg,${baseColor}10, ${baseColor}40)`, borderColor : baseColor+'40'}}>
 
                 <FilePenLineIcon className='size-7 group-hover:scale-105 transition-all' style={{color : baseColor }}/>
+                <br />
+
                 <p className='text-sm group-hover:scale-105 transition-all px-2 text-center' style={{color:baseColor}}>{resume.title}</p>
                 <p className='absolute bottom-1 text-[11px] text-slate-400 group-hover:text-slate-500 transition-all duration-300 px-2 texts-center' style={{color : baseColor+'90'}}>
                   Updated on {new Date(resume.updatedAt).toLocaleDateString()}
